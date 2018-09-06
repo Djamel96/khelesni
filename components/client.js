cd4ee7953ae25f5406a0d68303bfba5f3bb3e83f
Madani <fd_madani@esi.dz>
2018-09-06 22:31:20 +0100

    mlfk

--- components/client.js
+++ components/client.js
@@ -15,12 +15,14 @@ new Vue({
 		
 		methods:{
 			provideInfo: function (event) {
-			    console.log(this.email + this.password)
+			    
+			    console.log(this.email + this.password);
 			    var tab = new Array();
-			    tab.push(this.email)
-			    tab.push(this.password)
-			    console.log(tab)
-			    return tab
+			    tab.push(this.email);
+			    tab.push(this.password);
+				console.log(tab);
+				window.document.location.href='catalog.html';
+			    return tab;
 		    },
 		    goforgot : function (argument) {
       			window.document.location.href='forgot.html';
