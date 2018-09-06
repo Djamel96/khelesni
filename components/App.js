cea87c0f6c65d42ab81ea7517bf60a0a14befb6c
Madani <fd_madani@esi.dz>
2018-09-06 12:05:44 +0100

    m

--- components/App.js
+++ components/App.js
@@ -18,7 +18,7 @@ new Vue({
     tabContent : [
 
   { ID : 'about', 
-    title : 'About Us', 
+    title : 'A propos de nous', 
     description : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' , 
     color : '#F0EDE5',
     right : false,
@@ -26,7 +26,7 @@ new Vue({
   },
   {
     ID : 'how', 
-    title : 'How It Works', 
+    title : 'Comment ça marche  ', 
     description : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     color : '#B4B7BA',
     right : true,
@@ -38,8 +38,12 @@ new Vue({
       window.document.location.href='html/clientSignin.html';
     },
 
-    goFree : function (argument) {
-      window.document.location.href='html/freelancerSignin.html';
-    }
+    goArtisan : function (argument) {
+      window.document.location.href='html/ArtisanSignin.html';
+    },
+
+    goCatalog : function (argument) {
+      window.document.location.href='html/catalog.html';
+    },
   }
 })
